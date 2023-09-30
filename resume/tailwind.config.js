f495d02244815dec9db46f7f3106e6d678c2f4e9
Janine Fuchs <146532473+jfuchs-png@users.noreply.github.com>
2023-09-30 10:51:07 +0000

    Update 7 files

--- resume/tailwind.config.js
+++ resume/tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      serif: ['Georgia', 'serif'],
     },
     extend: {},
   },
